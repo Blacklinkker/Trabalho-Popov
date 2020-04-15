e84e8ff491cbd8c854d8e2ade75ab5a20bae8f1c
Blacklinkker <55569386+Blacklinkker@users.noreply.github.com>
2020-04-15 10:21:51 -0300

    Trab2+fila+no são todos do msm trab

--- Fila.java
+++ Fila.java
@@ -2,20 +2,34 @@ package Trab;
 
 public class Fila{
     //declara os marcadores
-    No cabeca=null;
+    //primeiro da fila
+    No primeiro=null;
+    //ultimo da fila (onde ela "começa")
     No inicio=null;
+    //tamanho da fila
     int tamanho=0;
 
-    //verifica se tem cabeça da fila
-    String verificaCabeca(){
-        if(cabeca==null){
+    //verifica se tem o primeiro cliente na fila
+    String verificaPrimeiro(){
+        //compara com nulo,se der diferente então temos o primeiro na fila
+        if(primeiro==null){
             return null;
         }else{
-            return cabeca.info;
+            return primeiro.info;
         }
     }
+    //mostra a quantidade de clientes e ordenados numa String, recebendo o numero de clientes
+    String filaEmOrdem(int numeroClientes){
+        //criação da String que guarda a fila
+        String filaOrdenada="(caixa eletronico está aqui)<== ";
+        //laço que ordena os clientes
+        for(int i=0;i<numeroClientes;i++){
+            filaOrdenada+=(i+1)+"º cliente"+", ";
+        }
+        return filaOrdenada;
+    }
 
-    //retorna se é vazia ou não
+    //retorna se é vazia ou não (se tamanho==0 = "true", de outra forma é "false")
     boolean vazia(){
         return tamanho==0;
     }
@@ -27,29 +41,31 @@ public class Fila{
         no.proximo=inicio;
         inicio=no;
         if(tamanho==0){
-            cabeca=no;
+            primeiro=no;
         }
         tamanho++;
     }
 
     //remove componentes da fila
     String retirar(){
-        //verifica se nao esta vazia
+        
+        //verifica se a fila do caixa não esta vazia
+        //utiliza o metodo ja criado para economia de linhas e praticidade
         if(vazia()){
             return null;
         }
-        String info=cabeca.info;
-        //verifica se tem apenas a cabeca, se sim zera ela
+        String info=primeiro.info;
+        //verifica se apenas tem uma pessoa na fila, se sim zera ela para remover.
         if(tamanho==1){
             inicio=null;
-            cabeca=null;
+            primeiro=null;
         }else{
             No local=inicio;
-            while(local.proximo!= cabeca){
+            while(local.proximo!= primeiro){
                 local=local.proximo;
             }
-            cabeca=local;
-            cabeca.proximo=null;
+            primeiro=local;
+            primeiro.proximo=null;
         }
         tamanho--;
         return info; 
