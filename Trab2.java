e62c108e93a06e9cfe25ea239ab3f7fbb02e32a3
Blacklinkker <55569386+Blacklinkker@users.noreply.github.com>
2020-04-15 10:20:41 -0300

    Comentado e adicionada a função "ordena clientes", nao mostra o nome ainda

--- Trab2.java
+++ Trab2.java
@@ -4,14 +4,29 @@ import javax.swing.JOptionPane;
 
 public class Trab2 {
     public static void main(String[] args) {
-        //adaptar para vetor como ele pede no trabalho
-        //precisamos da ideia de "problema" a ser resolvido
-        Fila f=new Fila();
-        f.inserir(JOptionPane.showInputDialog("Insira um valor"));
-        f.inserir(JOptionPane.showInputDialog("Insira outro valor"));   
-        f.inserir(JOptionPane.showInputDialog("Insira um valor"));
-        while(!f.vazia()){
-            JOptionPane.showMessageDialog(null, f.retirar());
+        
+        //fila sendo utilizada para armazenar um fila de banco em um caixa eletronico
+        
+        //estanciação da fila (que está  presente no arquivo "Fila.java")
+        Fila clientes=new Fila();
+       
+        //armazena quantos clientes estão na fila para utilizar no laço
+        int clientesFila=Integer.parseInt(JOptionPane.showInputDialog("insira a quantidade de clientes na fila: "));
+
+        //insere os clientes na fila utilizando o metodo cliente.inserir() definido em Fila.java
+        //e um laço de repetição com o numero de clientes presentes na fila
+        for(int i=0;i<clientesFila;i++){
+            clientes.inserir(JOptionPane.showInputDialog("Insira o nome do"+ (i+1)+"º cliente a chegar: "));
         }
+        //coloca os clientes em ordem em uma String para facilitar noção da fila
+        String ordenados=clientes.filaEmOrdem(clientesFila);
+        JOptionPane.showMessageDialog(null, ordenados);
+
+        //remove os clientes em ordem de chegada até não restar nenhum 
+        while(clientes.vazia()==false){
+            //anuncia que os clientes foram atendidos e solicita o proximo
+            JOptionPane.showMessageDialog(null, "O cliente "+clientes.retirar()+" acaba de ser atendido,que venha o proximo!");
+        }
+        JOptionPane.showMessageDialog(null, "Todos os clientes foram atendidos com sucesso!");
     }
 }
